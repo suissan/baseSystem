84a5087b1c2cd6edf3cb38716faa2380db25297d
suissan <programming0607@gmail.com>
2024-07-11 23:21:29 +0900

    htmlやテンプレートエンジンの設定を追加

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 "use strict";
 
-// express起動
+// express・その周辺の読み込み
 const express = require("express");
-const { getBaseInfo } = require("./get-baseinfo");
 const app = express();
+const pug = require("pug");
 
+// ベースの情報を取得するモジュールを読み込み
+const { getBaseInfo } = require("./get-baseinfo");
 
+//テンプレートエンジン
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+// 静的ファイルを使用可能に設定
+app.use(express.static('public'));
+
+// 起動ポート
 const PORT = 8000;
+
+apps.set()
 
 app.get("/", (req, res) => {
     res.send("hello world");
