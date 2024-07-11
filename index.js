528b9ffd190a7f1d261a76d71df089910cc29ac9
suissan <programming0607@gmail.com>
2024-07-11 22:34:05 +0900

    ベースマイページから情報を取得可能に変更

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 
 // express起動
 const express = require("express");
+const { getBaseInfo } = require("./get-baseinfo");
 const app = express();
+
 
 const PORT = 8000;
 
 app.get("/", (req, res) => {
     res.send("hello world");
+    getBaseInfo();
 })
 
 app.listen(PORT, () => {
